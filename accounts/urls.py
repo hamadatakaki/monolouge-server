6748a5cbeea4b7d04ad6eedd481bfa213bdb9258
hamadatakaki <i16hamadat@gmail.com>
2019-02-23 00:10:20 +0900

    :+1: implements following API

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 
 from rest_framework import routers
 
-from accounts.views import AccountViewSet
+from accounts.views import AccountViewSet, follow
 
 router = routers.DefaultRouter()
 router.register('accounts', AccountViewSet)
 
-urlpatterns = []
+urlpatterns = [
+    path('follow/', follow)
+]
